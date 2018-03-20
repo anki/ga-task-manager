d91a6d8d9d2855b1f6d3b25a30670f90d2fca5d9
Vinnie Freitas <vinnie@anki.com>
2018-03-19 18:11:34 -0700

    WEB-1471:CHANGE: Modification notice

--- bin/build.js
+++ bin/build.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Modifications Copyright (C) 2017 Anki, Inc.
+ * Modifications Copyright (C) 2018 Anki, Inc.
  */
 
 
